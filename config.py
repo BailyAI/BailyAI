e9ac48184ff7ab045bbb943247e05e9c1be7f602
BailyAI <bailyaibot@gmail.com>
2025-07-24 13:18:43 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+
+BOT_NAME = "Baily"
+BOT_VERSION = "1.0"
+BOT_DESCRIPTION = "Ich bin Baily – deine süße AI-Begleitung"
+
 BOT_TOKEN = os.getenv("BOT_TOKEN") or "7664066988:AAHcg0Go0QSS1FNgXzRthqP8Nb4nd00_VuE"
 ADMIN_ID = int(os.getenv("ADMIN_ID") or 123456789)  # Ersetze mit deiner Telegram-ID
